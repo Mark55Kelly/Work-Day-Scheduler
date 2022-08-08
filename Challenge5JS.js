a7560920b2c1402f2f75bde41bd92dff17ec05a1
Mark55Kelly <108310217+Mark55Kelly@users.noreply.github.com>
2022-08-08 08:37:07 -0500

    Add files via upload
    Added global variables to the file.

---
+++ Challenge5JS.js
@@ -0,0 +1,20 @@
+var text9AM = $('#textarea-9AM');
+var text10AM = $('#textarea-10AM');
+var text11AM = $('#textarea-11AM');
+var text12PM = $('#textarea-12PM');
+var text1PM = $('#textarea-1PM');
+var text2PM = $('#textarea-2PM');
+var text3PM = $('#textarea-3PM');
+var text4PM = $('#textarea-4PM');
+var text5PM = $('#textarea-5PM');
+
+var savebutton9AM = $('#saveBtn-9AM');
+var savebutton10AM = $('#saveBtn-10AM');
+var savebutton11AM = $('#saveBtn-11AM');
+var savebutton12PM = $('#saveBtn-12PM');
+var savebutton1PM = $('#saveBtn-1PM');
+var savebutton2PM = $('#saveBtn-2PM');
+var savebutton3PM = $('#saveBtn-3PM');
+var savebutton4PM = $('#saveBtn-4PM');
+var savebutton5PM = $('#saveBtn-5PM');
+
